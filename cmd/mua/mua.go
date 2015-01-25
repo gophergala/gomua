f81e3e512db2bbe499451345cc3d35b65e87e615
Andrew <danweasel@gmail.com>
2015-01-25 09:48:32 +0700

    pulled 95% of send.go cmd into new package gomua, for use in mua client, stubbed out some basic functions needed for mua client in mua.go

--- cmd/mua/mua.go
+++ cmd/mua/mua.go
@@ -1,4 +1,40 @@
 package main
 
+// takes a Maildir directory, scans for messages, and returns a slice of Message structs.
+func scanMailDir(dir string) (mail []*Message) {
+
+}
+
+// takes a slice of Messages and prints a numbered list of summaries
+func viewMail(mail []*Message) {
+
+}
+
+// prints a single mail message to the screen
+func viewMessage(msg *Message) {
+
+}
+
+// (to be invoked from viewMessage with a keypress, most likely)
+// create a new message:
+// put old's Message-ID into reply's In-Reply-To and References headers
+// put old's content as quoted material in reply
+// put old's From as the reply's To
+// put old's Subject in reply's subject as "RE: Subject" (but allow user to edit? -- later)
+// prompt user for content
+// send reply
+func replyMessage(old *Message) (reply *Message) {
+
+	return reply
+}
+
 func main() {
+	mail := fakeMailDir()
+	viewMail(mail)
+}
+
+// a list of static Messages for testing view
+func fakeMailDir() (mail []*Message) {
+
+	return mail
 }
---
+++ send.go
@@ -0,0 +1,141 @@
+package gomua
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/smtp"
+	"os/user"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const configLocation = "/.gomua/send.cfg"
+
+// SMTPServer describes a connection to an SMTP server for sending mail.
+type SMTPServer struct {
+	name     string
+	username string
+	password string
+	address  string
+	port     int
+	tlsB     bool
+}
+
+// NewSMTPServer reads from a configuration file, and returns a new SMTPServer struct ready to use.
+func NewSMTPServer(filename string) (*SMTPServer, error) {
+	s := new(SMTPServer)
+
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, errors.New("Missing ~/.gomua/send.cfg file.")
+	}
+
+	lines := strings.Split(string(b), "\n")
+
+	for _, l := range lines {
+		switch {
+		case strings.HasPrefix(l, "Name="):
+			s.name = strings.TrimPrefix(l, "Name=")
+		case strings.HasPrefix(l, "Username="):
+			s.username = strings.TrimPrefix(l, "Username=")
+		case strings.HasPrefix(l, "Password="):
+			s.password = strings.TrimPrefix(l, "Password=")
+		case strings.HasPrefix(l, "Address="):
+			s.address = strings.TrimPrefix(l, "Address=")
+		case strings.HasPrefix(l, "Port="):
+			s.port, _ = strconv.Atoi(strings.TrimPrefix(l, "Port="))
+		case strings.HasPrefix(l, "TLS="):
+			str := strings.TrimPrefix(l, "TLS=")
+			if str == "true" {
+				s.tlsB = true
+			} else {
+				s.tlsB = false
+			}
+		}
+	}
+
+	if s.name == "" || s.username == "" || s.password == "" || s.address == "" || s.port == 0 {
+		return nil, errors.New("Incorrect ~/.mua/send.cfg file.")
+	}
+	return s, nil
+}
+
+// Connects and authenticates to an SMTPServer, returns a client connection ready to write.
+// This client *must be Quit()ed after finished using, preferably with defer.
+func connectSMTP(s *SMTPServer) (*smtp.Client, error) {
+	c, err := smtp.Dial(fmt.Sprintf("%s:%d", s.address, s.port))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.StartTLS(&tls.Config{ServerName: s.name}); err != nil {
+		return nil, err
+	}
+
+	auth := smtp.PlainAuth("", s.username, s.password, s.address)
+	if err := c.Auth(auth); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// SendSMTP takes a SMTP server and a message, connects to the server, sends the message, and quits the connection to the server.
+func sendSMTP(server *SMTPServer, msg *Message) error {
+	// connect to SMTP server
+	var c *smtp.Client
+	c, err := connectSMTP(server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Quit()
+
+	// Set sender and receiver
+	if err := c.Mail(msg.from.Address); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, t := range msg.to {
+		if err := c.Rcpt(t.Address); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Send email body
+	wc, err := c.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+	msg.AddHeader(fmt.Sprintf("Date: %s%s", time.Now().Local().Format(time.RFC822), "\r\n"))
+	_, err = fmt.Fprintf(wc, "To: %v\nFrom: %v\nSubject: %v\n%v\n\n%v", msg.To(), msg.From(), msg.Subject(), msg.Headers(), msg.Content())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = wc.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+// Send opens a new SMTP server connection from the config file and sends a message.
+func Send(msg *Message) {
+	// Look for a SMTPServer configuration file in ~/.gomua/send.cfg
+	u, _ := user.Current()
+	srv, err := NewSMTPServer(u.HomeDir + configLocation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nSending...")
+
+	if sendSMTP(srv, msg) == nil {
+		fmt.Println("Message Sent")
+	}
+}
