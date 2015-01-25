27794af3b1c4b3a67e6689fb8c26479433afe4f7
Andrew <danweasel@gmail.com>
2015-01-25 12:13:01 +0700

    Added function to reply to emails.

--- cmd/mua/mua.go
+++ cmd/mua/mua.go
@@ -39,7 +39,23 @@ func viewMessage(msg *gomua.Message) {
 // prompt user for content
 // send reply
 func replyMessage(old *gomua.Message) (reply *gomua.Message) {
+	id := "121212"                             // should be parsed from old by hp.go eventually
+	oldref := ""                               // ditto
+	from := "Frenata <mr.k.frenata@gmail.com>" // mua.cfg for sending email address?
 
+	subject := fmt.Sprintf("RE: %s", old.Subject())
+	inreplyto := fmt.Sprintf("In-Reply-To: %s", id)
+	references := fmt.Sprintf("References: %s%s", oldref, id)
+	headers := []string{inreplyto, references}
+
+	content := gomua.WriteContent(os.Stdin)
+
+	quote := bufio.NewScanner(strings.NewReader(old.Content()))
+	for quote.Scan() {
+		content += "\n" + "> " + quote.Text()
+	}
+
+	reply = gomua.NewMessage(old.From(), from, subject, headers, content)
 	return reply
 }
 
@@ -58,6 +74,13 @@ func input(mail []*gomua.Message, exit chan bool) {
 		switch {
 		case input == "main":
 			viewMail(mail, os.Stdout)
+		case strings.HasPrefix(input, "reply"):
+			num, err := strconv.Atoi(strings.TrimPrefix(input, "reply "))
+			if err != nil {
+				fmt.Println(err)
+			}
+			reply := replyMessage(mail[num-1])
+			gomua.Send(reply)
 		case input == "exit", input == "x", input == "quit", input == "q":
 			exit <- true
 		case strings.ContainsAny(input, "01234566789"):
@@ -85,10 +108,10 @@ func main() {
 
 // a list of static Messages for testing view
 func fakeMailDir() (mail []*gomua.Message) {
-	m1 := gomua.NewMessage("mr.k.frenata@gmail.com", "danweasel@gmail.com", "Sample", []string{"Message-ID: 112233445566778899"}, "Sample message content")
+	m1 := gomua.NewMessage("mr.k.frenata@gmail.com", "danweasel@gmail.com", "Sample", []string{"Message-ID: 112233445566778899"}, "Sample message content\nNew line")
 	d, _ := time.ParseDuration("-2h")
 	m1.SetDate(time.Now().Add(d))
-	m2 := gomua.NewMessage("mr.k.frenata@gmail.com", "queenpuabi@gmail.com", "From Bekah", []string{"Message-ID: ihu491491"}, "Breakfast?")
+	m2 := gomua.NewMessage("mr.k.frenata@gmail.com", "dan.weasel@gmail.com", "DW", []string{"Message-ID: ihu491491"}, "Breakfast?")
 	m2.SetDate(time.Now())
 	mail = append(mail, m1)
 	mail = append(mail, m2)
